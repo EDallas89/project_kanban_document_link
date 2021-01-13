659ceb977740c372342cb10f8ad5d9563643994f
EDallas <inmsanz89@gmail.com>
2021-01-13 18:28:55 +0000

    First funtional commit

---
+++ README.rst
@@ -0,0 +1,11 @@
+Project Kanban Document Link
+=============================================
+
+    This module add a document link on project kanban view.
+
+Credits
+-------
+* Authors
+    * Aresoltec Canarias S.L.
+* Contributors
+    * Inma Sánchez
---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Project Kanban Document Link",
+    'summary': """
+        Add document link on project kanban view""",
+    'author': "Inma Sánchez",
+    'website': "https://github.com/EDallas89",
+    'category': 'Project',
+    'version': '1.0',
+    'depends': ['project'],
+    'data': [
+        'views/views.xml',
+    ],
+}
